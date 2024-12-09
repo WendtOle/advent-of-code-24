be09f8921147286120be671196bc37bb7974c567
Ole <ole@MBP-von-Ole.fritz.box>
2024-12-09 21:21:34 +0100

    Attempt for 06-2

--- 06/logic.py
+++ 06/logic.py
@@ -33,38 +33,51 @@ def walk_guard(puzzle):
 # and the output of 6 is only by accident 
 
 def mislead_the_guard(puzzle):
-    width = get_width(puzzle)
     cur_pos = get_guard_position(puzzle)
     cur_dir = Dir.UP
-    obstacles_bounced = []
-    here_obstacle_would_lead_to_loop = []
-    while True:
-        try: 
-            next_pos = next_position(cur_pos, width, cur_dir)
-        except Exception:
-            break
-        next_object = puzzle[next_pos]
-        next_dir_index = order.index(cur_dir) + 1
-        if next_dir_index >= 4:
-            next_dir_index = 0
-        next_dir_if_bounces = order[next_dir_index]
-        if next_object == "#":
-            cur_dir = next_dir_if_bounces
-            obstacles_bounced.append(next_pos)
-        else:
-            if would_bounce_on_already_meet_object(puzzle, obstacles_bounced,next_dir_if_bounces, cur_pos):
-                here_obstacle_would_lead_to_loop.append(next_pos)
 
-        try: 
-            next_pos = next_position(cur_pos, width, cur_dir)
-        except Exception:
-            break
-        cur_pos = next_pos
-    print(obstacles_bounced, here_obstacle_would_lead_to_loop)
-    for pos in here_obstacle_would_lead_to_loop:
-        print(int(pos % width), int(pos // width))
-    print(len(here_obstacle_would_lead_to_loop))
-    
+    output = recursive(puzzle, cur_pos, cur_dir)
+    print(output)
+
+    """
+    for path in output:
+        for pos in path:
+            print(int(pos % width), int(pos // width))
+        print()"""
+    return len(output)
+
+def recursive(puzzle, current_position, current_direction, obstacles_bounced = [], possible_obsticle_place_was_set = None):
+    width = get_width(puzzle)
+    next_pos = None
+    try: 
+        next_pos = next_position(current_position, width, current_direction)
+    except Exception:
+        return []
+    paths = []
+    next_object = puzzle[next_pos]
+    if next_pos in obstacles_bounced and possible_obsticle_place_was_set != None :
+        return [obstacles_bounced]
+    if next_object == "#":
+        path_when_bounced = recursive(puzzle, current_position, next_direction(current_direction), obstacles_bounced + [next_pos], possible_obsticle_place_was_set)
+        for path in path_when_bounced:
+            paths.append(path)
+    else:
+        if possible_obsticle_place_was_set == None:
+            path_if_woud_bounced = recursive(puzzle, current_position, next_direction(current_direction), obstacles_bounced + [next_pos], next_pos)
+            for path in path_if_woud_bounced:
+                paths.append(path)
+        path_if_would_not_bounced = recursive(puzzle, next_pos, current_direction, obstacles_bounced, possible_obsticle_place_was_set)
+        for path in path_if_would_not_bounced:
+            paths.append(path)
+    return paths
+
+def next_direction(cur_dir):
+    next_dir_index = order.index(cur_dir) + 1
+    if next_dir_index >= 4:
+        next_dir_index = 0
+    next_dir_if_bounces = order[next_dir_index]   
+    return next_dir_if_bounces 
+
 def would_bounce_on_already_meet_object(puzzle, obstacles_bounced, direction,cur_pos):
     width = get_width(puzzle)
     if len(obstacles_bounced) == 0:
@@ -111,12 +124,13 @@ step = {
 
 
 def next_position(cur, width, dir: Dir):
-    
     column = cur % width
     row = int(cur / width)
-    
+    #print(column, row)
     target_column = step[dir][0] + column
     target_row = step[dir][1] + row
+
+    #print(target_column, target_row)
 
     if target_column < 0 or target_column >= width or target_row < 0 or target_row >= width: 
         raise Exception("out of bounds")
@@ -125,10 +139,11 @@ def next_position(cur, width, dir: Dir):
 
 
 if __name__ == "__main__":
-    puzzle = read_input_from_file("./given-input.txt")
+    puzzle = read_input_from_file("./puzzle-input.txt")
     
     #path = walk_guard(puzzle)
     #distinct = list(set(path))
     #print(len(distinct))
 
-    mislead_the_guard(puzzle)
+    output = mislead_the_guard(puzzle)
+    print(output)
---
+++ 06/test_logic.py
@@ -0,0 +1,20 @@
+from logic import mislead_the_guard
+from load_input import read_input_from_file
+
+def test_bounces_on_not_previously_seen_obsticle():
+    input = """
+    ......
+    ...#..
+    .....#
+    ......
+    ..#^..
+    ......
+    """
+    output = mislead_the_guard("".join(map(lambda line: line.strip(), input)))
+    print("output", output)
+    assert output == 1
+
+def test_given_example():
+    puzzle = read_input_from_file("./given-input.txt")
+    output = mislead_the_guard(puzzle)
+    assert output == 6
