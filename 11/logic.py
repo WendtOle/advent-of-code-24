c0e9a871e360a4e0e1806f0e3bec3f1b620507e2
Ole <ole@MBP-von-Ole.fritz.box>
2024-12-12 23:22:00 +0100

    Make 14 times faster and solve 11-2
    first improvement brought 6 times improvement
    second 14
    
    calculating 75 blinks took me around 95 seconds

--- 11/logic.py
+++ 11/logic.py
@@ -1,25 +1,35 @@
-def transform_stone(engraving):
+def transform_stone(stone):
+    [engraving, multiplier] = stone
     if engraving == 0:
-        return [1]
+        return [(1,multiplier)]
     engraving_as_string = str(engraving)
     if len(engraving_as_string) % 2 == 0:
         middle_index = int(len(engraving_as_string) / 2)
-        return [int(engraving_as_string[:middle_index]), int(engraving_as_string[middle_index:])]
-    return [engraving * 2024]
+        return [(int(engraving_as_string[:middle_index]),multiplier), (int(engraving_as_string[middle_index:]),multiplier)]
+    return [(engraving * 2024,multiplier)]
 
 def blink(stones, stone_memory):
     stones_after_blink = []
     for stone in stones:
+        stones_to_add = None
         if stone in stone_memory:
-            stones_after_blink += stone_memory[stone]
-            continue
-        transformed_stones = transform_stone(stone)
-        stone_memory[stone] = transformed_stones
-        stones_after_blink += transformed_stones
+            stones_to_add = stone_memory[stone]
+        else:
+            stones_to_add = transform_stone(stone)
+            stone_memory[stone] = stones_to_add
+        left = stones_to_add[0]
+        if left in stones_after_blink:
+            index = stones_after_blink.index(left)
+            [engraving, multiplier] = stones_after_blink[index] 
+            stones_after_blink[index] = (engraving, multiplier + left[1])
+            if len(stones_to_add) == 2:
+                stones_after_blink += [stones_to_add[1]]
+        else:
+            stones_after_blink += stones_to_add
     return [stones_after_blink, stone_memory]
 
 def blink_often(stones,times):
-    transformed_stones = stones
+    transformed_stones = list(map(lambda stone: (stone, 1), stones))
     stone_memory = {}
     for i in range(times):
         if i % 5 == 0:
@@ -27,7 +37,10 @@ def blink_often(stones,times):
         [updated_transformed_stones, updated_stone_memory] = blink(transformed_stones, stone_memory)
         transformed_stones = updated_transformed_stones
         stone_memory = updated_stone_memory
-    return len(transformed_stones)
+    counter = 0
+    for entry in transformed_stones:
+        counter += entry[1]
+    return counter
 
 import time
 
@@ -35,7 +48,7 @@ if __name__ == "__main__":
     input = [2701, 64945, 0, 9959979, 93, 781524, 620, 1]
 
     start_time = time.time() 
-    print(blink_often(input,40))
+    print(blink_often(input,75))
     end_time = time.time()
     print("time elapsed", end_time - start_time)
 
--- 11/test_logic.py
+++ 11/test_logic.py
@@ -1,19 +1,20 @@
 from logic import transform_stone, blink, blink_often
 
 def test_first_rule():
-    assert transform_stone(0) == [1]
+    assert transform_stone((0,1)) == [(1,1)]
 
 def test_second_rule():
-    assert transform_stone(10) == [1, 0]
+    assert transform_stone((10,1)) == [(1,1), (0,1)]
 
 def test_third_rule():
-    assert transform_stone(9) == [9 * 2024]
+    assert transform_stone((9,1)) == [(9 * 2024,1)]
 
 def test_given_example():
-    assert blink([125, 17]) == [253000, 1, 7]
-    assert blink([253000, 1, 7]) == [253, 0, 2024, 14168]
+    assert blink([(125,1), (17,1)],{})[0] == [(253000,1), (1,1), (7,1)]
+    assert blink([(253000,1), (1,1), (1,1)],{})[0] == [(253,1), (0,1), (2024,2)]
 
 def test_sum():
-    #assert blink_often([0], 2) == 1
+    assert blink_often([0], 2) == 1
     assert blink_often([125, 17],6) == 22
-    #assert blink_often([125, 17],25) == 55312
+    assert blink_often([10, 2],7) == 37
+    assert blink_often([125, 17],25) == 55312
