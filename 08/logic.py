27087b5af075079798a60ec624cdf53fa251c671
Ole <ole@MBP-von-Ole.fritz.box>
2024-12-14 16:40:50 +0100

    Solution for 08-2

--- 08/logic.py
+++ 08/logic.py
@@ -24,14 +24,25 @@ def generate_anti_node_positions(antennas, dimensions):
     for [left, right] in antenna_pairs:
         [x_left, y_left] = left
         [x_right, y_right] = right
-        x_step = x_left - x_right
-        y_step = y_left - y_right
-        left_antinode = (x_left + x_step,y_left + y_step)
-        right_antinode = (x_right - x_step,y_right - y_step)
-        if in_raster(left_antinode, dimensions):
-            antinodes.append(left_antinode)
-        if in_raster(right_antinode, dimensions):
-            antinodes.append(right_antinode)
+        x_step = x_right - x_left
+        y_step = y_right - y_left
+
+        antinodes += [left, right]
+        antinodes += generate_antinodes(left, (x_step * -1, y_step * -1), dimensions)
+        antinodes += generate_antinodes(right, (x_step, y_step), dimensions)
+    return antinodes
+
+def generate_antinodes(origin, step, dimensions):
+    [x_step, y_step] = step
+    antinodes = []
+    current = origin
+    while True:
+        [x,y] = current
+        new_node = (x + x_step,y + y_step)
+        if not in_raster(new_node, dimensions):
+            break;
+        antinodes.append(new_node)
+        current = new_node
     return antinodes
 
 def in_raster(position, dimension):
@@ -46,30 +57,37 @@ def in_raster(position, dimension):
 def generate_all_antinodes(file_name):
     [antennas, dimensions] = get_antenna_locations(file_name)
     antinodes = []
-    print_positions(antennas, [], dimensions)
     for key in antennas.keys():
         antinodes += generate_anti_node_positions(antennas[key], dimensions)
     unique = list(set(antinodes))
+    print_positions(antennas, unique, dimensions)
+    print(antennas)
+    print(unique)
     return len(unique)
 
 def print_positions(antenna_positions, antinode_positions, dimensions):
     [width, height] = dimensions
     for y in range(height):
         for x in range(width):
-            print(get_symbol_for_pos((x,y), antenna_positions),end="")
+            print(get_symbol_for_pos((x,y), antenna_positions, antinode_positions),end="")
         print("",end="\n")
 
-def get_symbol_for_pos(pos, antenna_positions):
+def get_symbol_for_pos(pos, antenna_positions, antinode_positions):
     [x,y] = pos
     for key in antenna_positions.keys():
         for entry in antenna_positions[key]:
             if entry[0] == x and entry[1] == y:
                 return key
+    for entry in antinode_positions:
+        if entry[0] == x and entry[1] == y:
+            return "#"
     return "."
 
 def test_generate_anti_node_positions():
-    assert generate_all_antinodes("./example.txt") == 14
-    assert False == True
+    assert generate_all_antinodes("./example.txt") == 34
+
+def test_generate_anti_node_positions_2():
+    assert generate_all_antinodes("./another-example.txt") == 9
 
 if __name__ == "__main__":
     print(generate_all_antinodes("./given.txt"))
