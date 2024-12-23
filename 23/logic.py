b1587132f2a72fdc65965186e8287d50e3e2f016
Ole Wendt <ole.wendt@lbd.de>
2024-12-23 13:25:24 +0100

    Solution for 23-2

--- 23/logic.py
+++ 23/logic.py
@@ -4,37 +4,40 @@ def main():
     with open("./puzzle.txt", "r") as file:
         lines = file.readlines()
     lines = list(map(lambda line: line.strip(), lines))
+
+    unique_connections = set()
+    for network in lines:
+        [left, right] = sorted(network.split("-"))
+        unique_connections.add(f"{left}-{right}")
+
     lookup = {}
-    with_target = set()
-    for connection in lines:
-        entries = connection.split("-")
-        entries.sort()
-        [left, right] = entries
+    for network in unique_connections:
+        entries = network.split("-")
         for entry in entries:
             if not entry in lookup:
                 lookup[entry] = []
-            lookup[entry].append({left, right})
-        if left.startswith("t") or right.startswith("t"):
-            with_target.add(f"{left}-{right}")
-    print(with_target, len(with_target))
+            other_entry = entries[0] if entries[0] != entry else entries[1]
+            lookup[entry].append(other_entry)
+    #print(lookup)
 
-    tuples_of_three_with_target = set()
+    networks = unique_connections
+    new_networks = None
+    while new_networks is None or len(new_networks) > 0 :
+        if not (new_networks is None):
+            networks = new_networks
+        new_networks = set()
+        for network in networks:
+            parts = network.split("-")
+            potential_new_partners = set(lookup[parts[0]])
+            for other_entry in parts[1:]:
+                potential_new_partners.remove(other_entry)
+                potential_new_partners = potential_new_partners.intersection(lookup[other_entry])
+            for new_partner in potential_new_partners:
+                sorted_partners = sorted([*parts, new_partner])
+                new_networks.add("-".join(sorted_partners))
+    print(networks, len(networks))
 
-    for tuple in with_target:
-        [left, right] = tuple.split("-")
-        potential_partners = []
-        for entry_tuple in lookup[left]:
-            [first, second] = entry_tuple
-            potential_partners.append(first if first != left else second)
-        for entry_tuple in lookup[right]:
-            [first, second] = entry_tuple
-            to_compare = first if first != right else second
-            if to_compare in potential_partners:
-                array = [left, right, to_compare]
-                array.sort()
-                [start, middle, end] = array
-                tuples_of_three_with_target.add(f"{start}-{middle}-{end}")
-    print(tuples_of_three_with_target, len(tuples_of_three_with_target))
+
 
 if __name__ == "__main__":
     main()
